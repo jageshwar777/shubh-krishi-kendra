1555a0a3a95e6faeebc65450dd1676ef40267a36
jageshwar777 <jageshwarverma777@gmail.com>
2026-06-12 21:47:05 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,17 +1,4 @@
 pluginManagement {
-    val flutterSdkPath = run {
-        val properties = java.util.Properties()
-        file("local.properties").inputStream().use { properties.load(it) }
-        val flutterSdkPath = properties.getProperty("flutter.sdk")
-        if (flutterSdkPath != null) {
-            return@run flutterSdkPath
-        } else {
-            return@run System.getenv("FLUTTER_HOME") ?: "/usr/local/flutter"
-        }
-    }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
     repositories {
         google()
         mavenCentral()
