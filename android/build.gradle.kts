b9b3b5790e6b82942c992ea0e0eb36eef3e2763e
jageshwar777 <jageshwarverma777@gmail.com>
2026-06-12 20:21:36 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,31 +1,31 @@
+buildscript {
+    ext.kotlin_version = '1.9.0'
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.9.1'  // ✅ 8.7.0 से 8.9.1 करो
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
 allprojects {
     repositories {
         google()
-        mavenCentral() // यहाँ स्पेलिंग बिल्कुल सही कर दी गई है!
-    }
-    
-    // यह नियम ग्रेडल को सही वर्शन्स पर रोक कर रखेगा
-    configurations.all {
-        resolutionStrategy {
-            force("androidx.activity:activity:1.9.3")
-            force("androidx.activity:activity-ktx:1.9.3")
-            force("androidx.core:core:1.13.1")
-            force("androidx.core:core-ktx:1.13.1")
-        }
+        mavenCentral()
     }
 }
 
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
+rootProject.buildDir = '../build'
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
+    project.buildDir = "${rootProject.buildDir}/${project.name}"
 }
 subprojects {
-    project.evaluationDependsOn(":app")
+    project.evaluationDependsOn(':app')
 }
 
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+tasks.register('clean', Delete) {
+    delete rootProject.buildDir
 }
